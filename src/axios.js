f952f5a25621b67f18457de86548d57266629922
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2022-12-12 17:18:00 +0700

    D

--- src/axios.js
+++ src/axios.js
@@ -10,7 +10,7 @@ instance.interceptors.response.use(
     (response) => {
         // Thrown error for request with OK status code
         const { data } = response;
-        return response.data;
+        return data;
     }
 );
 
